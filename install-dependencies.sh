c35e59c5fbda8a420fd26fa338f5d448e6d849a3
rubicks <rubicks00@gmail.com>
2015-01-25 20:29:24 -0500

    refactors install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,65 +6,43 @@
 
 system=`uname -s`
 
+_pkgcmd=""
+
 case $system in
     Linux)
-        echo -n                                         && \
-            sudo apt-get -qq update                     && \
-            sudo apt-get -qq install libX11-dev         && \
-            sudo apt-get -qq install libfltk1.3-dev     && \
-            sudo apt-get -qq install libfontconfig1-dev && \
-            sudo apt-get -qq install libglu1-mesa-dev   && \
-            sudo apt-get -qq install libice-dev         && \
-            sudo apt-get -qq install libjpeg-dev        && \
-            sudo apt-get -qq install libjpeg62-dev      && \
-            sudo apt-get -qq install libpng-dev         && \
-            sudo apt-get -qq install libpng12-dev       && \
-            sudo apt-get -qq install libx11-dev         && \
-            sudo apt-get -qq install libxcursor-dev     && \
-            sudo apt-get -qq install libxext-dev        && \
-            sudo apt-get -qq install libxft-dev         && \
-            sudo apt-get -qq install libxi-dev          && \
-            sudo apt-get -qq install libxinerama-dev    && \
-            sudo apt-get -qq install libxrender-dev     && \
-            sudo apt-get -qq install libz-dev           && \
-            sudo apt-get -qq install mesa-common-dev    && \
-            sudo apt-get -qq install mingw32            && \
-            sudo apt-get -qq install mingw32-binutils   && \
-            sudo apt-get -qq install mingw32-runtime    && \
-            sudo apt-get -qq install wine               && \
-            sudo apt-get -qq install zlib1g-dev         && \
-            echo "${BASH_SOURCE} win"
+        _pkgcmd="sudo apt-get -y "
         ;;
     Darwin)
-        echo -n                             && \
-            brew update                     && \
-            brew install libX11-dev         && \
-            brew install libfltk1.3-dev     && \
-            brew install libfontconfig1-dev && \
-            brew install libglu1-mesa-dev   && \
-            brew install libice-dev         && \
-            brew install libjpeg-dev        && \
-            brew install libjpeg62-dev      && \
-            brew install libpng-dev         && \
-            brew install libpng12-dev       && \
-            brew install libx11-dev         && \
-            brew install libxcursor-dev     && \
-            brew install libxext-dev        && \
-            brew install libxft-dev         && \
-            brew install libxi-dev          && \
-            brew install libxinerama-dev    && \
-            brew install libxrender-dev     && \
-            brew install libz-dev           && \
-            brew install mesa-common-dev    && \
-            brew install mingw32            && \
-            brew install mingw32-binutils   && \
-            brew install mingw32-runtime    && \
-            brew install wine               && \
-            brew install zlib1g-dev         && \
-            echo "${BASH_SOURCE} win"
+        _pkgcmd="brew "
         ;;
     *)
         echo "unsupported system: $system"
         exit 1
+        ;;
 esac
+
+echo -n &&
+    eval ${_pkgcmd} update && \
+    eval ${_pkgcmd} install \
+         libX11-dev         \
+         libfltk1.3-dev     \
+         libfontconfig1-dev \
+         libglu1-mesa-dev   \
+         libice-dev         \
+         libjpeg-dev        \
+         libjpeg62-dev      \
+         libpng-dev         \
+         libpng12-dev       \
+         libxcursor-dev     \
+         libxext-dev        \
+         libxft-dev         \
+         libxi-dev          \
+         libxinerama-dev    \
+         libxrender-dev     \
+         libz-dev           \
+         mesa-common-dev    \
+         mingw32            \
+         mingw32-binutils   \
+         mingw32-runtime    \
+         wine
 
