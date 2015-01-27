5fdeea4d6c9ee44d70d1a8b04126d347f61a96f5
rubicks <rubicks00@gmail.com>
2015-01-27 18:57:43 -0500

    darwin hates function templates

--- src/Bitmap.cxx
+++ src/Bitmap.cxx
@@ -37,7 +37,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 namespace
 {
-  int _abs( int n ){ return ::std::abs<int>( n ); }
+  int _abs( int n ){ return ::std::abs( n ); }
 
 
   const int stack_size = 0x100000;
